783bf78f4fa0bac348fd678f4d6d387519be6d09
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-05-14 19:35:27 +0300

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,6 +14,14 @@ urlpatterns = [
 
     # Documentation
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/v1/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path(
+        'api/v1/docs/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui',
+    ),
+    path(
+        'api/v1/redoc/',
+        SpectacularRedocView.as_view(url_name='schema'),
+        name='redoc',
+    ),
 ]
