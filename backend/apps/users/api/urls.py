d1d53ba126359ecf20282b4920ec7bcf6d9d2cd6
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-05-17 22:21:29 +0300

    Update urls.py

--- backend/apps/users/api/urls.py
+++ backend/apps/users/api/urls.py
@@ -7,10 +7,18 @@ from apps.users.api import views
 
 
 router = DefaultRouter()
-router.register(r'users', views.AdminUserViewSet, basename='admin-users')
+router.register(r"users", views.AdminUserViewSet, basename="admin-users")
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("", include(router.urls)),
+    path(
+        "api/token/",
+        views.CustomTokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    path(
+        "api/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh"
+    ),
 ]
