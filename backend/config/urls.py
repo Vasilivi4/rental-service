241305814cea6019d92cc9632de277bb9a365b61
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-05-17 20:49:46 +0300

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,24 +4,24 @@ from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
-    SpectacularRedocView
+    SpectacularRedocView,
 )
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/users/', include('apps.users.api.urls')),
+from django.shortcuts import redirect
 
-    # Documentation
-    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+urlpatterns = [
+    path("", lambda request: redirect("api/v1/docs/")),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("apps.apartments.urls")),
+    path("api/v1/users/", include("apps.users.api.urls")),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        'api/v1/docs/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui',
+        "api/v1/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
     ),
     path(
-        'api/v1/redoc/',
-        SpectacularRedocView.as_view(url_name='schema'),
-        name='redoc',
+        "api/v1/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
     ),
 ]
