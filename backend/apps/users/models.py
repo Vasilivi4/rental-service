311ba4d4b6e19e7c6b3f1e95149b98bc6f2d9346
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-05-17 22:21:37 +0300

    Update models.py

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('Email address must be set')
+            raise ValueError("Email address must be set")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -13,19 +13,20 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
         return self.create_user(email, password, **extra_fields)
-    
+
+
 class User(AbstractUser):
     username = None
 
     email = models.EmailField(unique=True)
     is_verified = models.BooleanField(default=False)
-    auth_provider = models.CharField(max_length=20, default='email')
+    auth_provider = models.CharField(max_length=20, default="email")
 
-    USERNAME_FIELD = 'email' 
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = UserManager()
