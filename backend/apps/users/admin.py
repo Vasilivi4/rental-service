b031c8be8c3b4d892970af598417b1ddd66e3f5d
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-05-17 22:21:22 +0300

    Update admin.py

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -5,21 +5,49 @@ from apps.users.models import User
 
 @admin.register(User)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ['email', 'is_verified', 'is_staff', 'is_active', 'date_joined']
-    list_filter = ['is_verified', 'is_staff', 'is_active']
-    search_fields = ['email']
-    ordering = ['email']
+    list_display = [
+        "email",
+        "is_verified",
+        "is_staff",
+        "is_active",
+        "date_joined"
+    ]
+    list_filter = [
+        "is_verified",
+        "is_staff",
+        "is_active"
+    ]
+    search_fields = ["email"]
+    ordering = ["email"]
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_verified', 'is_staff', 'is_superuser', 'is_active')}),
-        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {"fields": (
+                "is_verified",
+                "is_staff",
+                "is_superuser",
+                "is_active"
+                )},
+        ),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_verified', 'is_staff', 'is_active'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_verified",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
     )
     filter_horizontal = ()
-    ordering = ('email',)
+    ordering = ("email",)
