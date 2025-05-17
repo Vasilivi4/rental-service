61a0038cffab7d7d993c29b58cbc3f69ab561c59
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-05-17 20:49:04 +0300

    Update admin.py

--- backend/apps/apartments/admin.py
+++ backend/apps/apartments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Apartment
 
-# Register your models here.
+
+@admin.register(Apartment)
+class ApartmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "availability", "owner", "created_at")
+    list_filter = ("availability", "number_of_rooms", "price")
+    search_fields = ("name", "description", "owner__username")
+    prepopulated_fields = {"slug": ("name",)}
